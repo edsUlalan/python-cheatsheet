3e31e86269beb0238bd2294462d37db711d4fb33
edsUlalan <edds.ulalan@gmail.com>
2021-08-07 11:25:00 +0800

    add string interpolation example

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,7 @@
-myfirstvarialble = "Hello World"
-print(myfirstvariable)
+message = "Hello World"
+print(message)
+
+first_name = 'software'
+last_name = 'developer'
+full_name = f"{first_name} {last_name}"
+print(full_name)
