4421e02dcbb6129d395c38c0782b95344234ce34
edsUlalan <edds.ulalan@gmail.com>
2021-08-08 20:34:45 +0800

    add input syntax examples

---
+++ input.py
@@ -0,0 +1,12 @@
+# prompting for a value
+name = input("What's your name? ")
+print(f"Hello, {name}!")
+
+# prompting for a numerical input
+age = input("How old are you? ")
+age = int(age)
+print(f"You are {age} year old")
+
+pi = input("What is the value of pi? ")
+pi = float(pi)
+print(f"The value of Pi is equal to {pi}")
