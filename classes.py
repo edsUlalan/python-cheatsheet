8468825fc76f82bd110e42191a394fb3316944ed
edsUlalan <edds.ulalan@gmail.com>
2021-08-09 14:26:13 +0800

    add class examples

---
+++ classes.py
@@ -0,0 +1,33 @@
+# dog class example
+class Dog():
+    """Represent a dog."""
+
+    def __init__(self, name):
+        """Initialize dog object."""
+        self.name = name
+
+    def sit(self):
+        """Simulate sitting."""
+        print(f"{self.name} is sitting.")
+
+my_dog = Dog('Milo')
+print(f"{my_dog.name} is a great dog!")
+my_dog.sit()
+
+# inheritance
+class SARDog(Dog):
+    """Represent a search dog."""
+
+    def __init__(self, name):
+        """Initialize the sardog."""
+        super().__init__(name)
+
+    def search(self):
+        """Simulate searching."""
+        print(f"{self.name} is searching.")
+
+my_dog = SARDog('Willie')
+
+print(f"{my_dog.name} is a search dog.")
+my_dog.sit()
+my_dog.search()
