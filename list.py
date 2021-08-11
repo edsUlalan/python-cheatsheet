043068089f7154d5a67e62163dde50f3194db98f
edsUlalan <edds.ulalan@gmail.com>
2021-08-11 22:58:30 +0800

    add more examples to list

--- list.py
+++ list.py
@@ -62,17 +62,62 @@ for x in range(1,11):
     squares.append(x ** 2)
 print(squares) # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
+cubes = []
+for y in range(100):
+    cubes.append(y ** 3)
+print(cubes)
+
 # list comprehensions
 squares = [x**2 for x in range(1,11)]
+
+title = ['one', 'two', 'three']
+convert_upper =  [t.upper() for t in title]
+
+# making a list of numbers from 1 to a million
+numbers = list(range(1, 1000001))
+
+# using a loop to convert a list of names to upper case
+names = ['kai', 'abe', 'ada', 'gus', 'zoe']
+upper_names = []
+for name in names:
+    upper_names.append(name.upper())
+print(upper_names)
 
 # slicing a list
 finishers = ['sam', 'bob', 'ada', 'bea']
 first_two = finishers[:2]
 print(first_two) # ['sam', 'bob']
 
+middle_two = finishers[2:3]
+print(middle_two) # ['bob', 'ada']
+
+last_three = finishers[-3:]
+print(last_three) # ['bea', 'ada', 'bob']
+
 # copy a list of bikes
 copy_of_bikes = bikes[:]
 print(copy_of_bikes) # ['trek', 'redline', 'giant', 'specialize']
 
+# finding the minimum value in a list
+ages = [6, 7, 1, 2, 3, 4, 5]
+youngest = min(ages)
+
+# finding the maximum
+ages = [6, 7, 1, 2, 3, 4, 5]
+oldest = max(ages)
+
+# finding the sum of all values
+ages = [6, 7, 1, 2, 3, 4, 5]
+total_years = sum(ages)
+
 # tuples - similar to list but can't be modified
 dimensions = (1920, 1080)
+
+# looping through a tuple
+for dimension in dimensions:
+    print(dimension)
+
+# overwriting a tuple
+sides = (100, 200)
+print(sides)
+sides = (200, 120)
