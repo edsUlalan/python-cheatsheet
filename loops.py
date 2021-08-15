c0ef4856b4942098b55b6375a1e83b845c97969e
edsUlalan <edds.ulalan@gmail.com>
2021-08-15 21:49:34 +0800

    add more examples to while loops with conditional statements

--- loops.py
+++ loops.py
@@ -9,3 +9,64 @@ message = ''
 while message != 'quit':
     message = input("What's your message? ")
     print(message)
+
+# using a flag
+prompt = "\nTell me something, and I'll "
+prompt += "repeat it back to you."
+prompt += "\nEnter 'quit' to end the program."
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
+# using break to exit a loop
+prompt = "\nWhat cities have you visited?"
+prompt += "\nEnter 'quit' when you're done."
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(f"I've been to {city}!")
+
+# using continue on a loop
+banned_users = ['eve', 'fred', 'gary', 'helen']
+
+prompt = "\nAdd a player to your team."
+prompt += "\nEnter 'quit' when you're done."
+
+players = []
+while True:
+    player = input(prompt)
+    if player == 'quit':
+        break
+    elif player in banned_users:
+        print(f"{player} is banned!")
+        continue
+    else:
+        players.append(player)
+
+print("\nYour team:")
+for player in players:
+    print(player)
+
+# avoiding infinite loop like this
+while True:
+    name = input("\nWho are you? ")
+    print(f"Nice to meet you, {name}!")
+
+# removing all cats from a list of pets
+pets = ['dog', 'cat', 'dog', 'fish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
