95e4e1643e9cd15a0d8af6376cbefa892dbe22d7
edsUlalan <edds.ulalan@gmail.com>
2021-08-15 21:15:12 +0800

    add more examples to conditional statements

--- statements.py
+++ statements.py
@@ -9,8 +9,17 @@ x <= 42 # less or equal => true
 
 # conditional test with list
 bikes = ['one', 'two', 'three']
-'one' in bikes # true
-'four' not in bikes # true
+'one' in bikes # True
+'four' not in bikes # True
+
+# checking if a list is empty
+players = []
+
+if players:
+    for player in players:
+        print(f"Player: {player.title()}")
+else:
+    print("We have no players yet!")
 
 # Assigning boolean values
 active = True
@@ -27,3 +36,26 @@ elif age < 18:
     ticket_price = 10
 else:
     ticket_price = 15
+
+# more examples on checking equality
+car = 'bmx'
+car == 'bmx' # True
+
+color = 'red'
+color == 'blue' # False
+
+# ignoring case when making comparison
+bird = 'Eagle'
+bird.lower() == 'eagle' # True
+
+# checking for inequality
+topping = 'mushrooms'
+topping != 'anchovies' # True
+
+# using and to check multiple conditions
+age_0 = 22
+age_1 = 18
+age_0 >= 21 and age_1 >= 21 # False
+
+age_2 = 23
+age_0 >= 21 and age_2 >= 21 # True
