93c2eff2b5e31099c3331ce0f84313473c2deaff
edsUlalan <edds.ulalan@gmail.com>
2021-08-19 13:25:28 +0800

    add more examples using functions with arguments

--- functions.py
+++ functions.py
@@ -20,6 +20,16 @@ def make_pizza(topping='bacon'):
 make_pizza()
 make_pizza('pepperoni')
 
+# using None to make an argument optional
+def describe_pet(animal, name=None):
+    """Display information about a pet."""
+    print(f"\nI have a {animal}.")
+    if name:
+        print(f"Its name is {name}.")
+
+describe_pet('hamster', 'harry')
+describe_pet('snake')
+
 # returning a value
 def add_numbers(x, y):
     """Add two numbers and return the sum."""
@@ -27,3 +37,16 @@ def add_numbers(x, y):
 
 sum = add_numbers(3, 5)
 print(sum)
+
+# using positional arguments
+def describe_pet(animal, name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal}.")
+    print(f"Its name is {name}.")
+
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
+
+# using keyword arguments
+describe_pet(animal='monkey', name="kiwew")
+describe_pet(name='chow', animal='cat')
