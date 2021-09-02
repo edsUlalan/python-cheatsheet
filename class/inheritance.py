e9bece1eba61be6b1e08faa960df4b9b3a86500f
edsUlalan <edds.ulalan@gmail.com>
2021-09-02 10:27:18 +0800

    add examples on method child class

--- class/inheritance.py
+++ class/inheritance.py
@@ -1,3 +1,23 @@
+class Car:
+    def __init__(self, make, model, year):
+        """Initialize car attributes."""
+        self.make = make
+        self.model = model
+        self.year = year
+
+        self.fuel_capacity = 15
+        self.fuel_level = 0
+
+    def fill_tank(self):
+        """Fill gas tank to capacity."""
+        self.fuel_level = self.fuel_capacity
+        print("Fuel tank is full.")
+
+    def drive(self):
+        """Simulate driving."""
+        print("The car is moving.")
+
+
 # inherit from class Car
 class ElectricCar(Car):
     """A simple model of an electric car."""
@@ -11,3 +31,17 @@ class ElectricCar(Car):
         self.battery_size = 75
         # Charge level in %.
         self.charge_level = 0
+
+# adding new methods to the child class
+class ElectricCar(Car):
+    --snip--
+    def charge(self):
+        """Fully charge the vehicle."""
+        self.charge_level = 100
+        print("The vehicle is fully charged.")
+
+# using child methods and parent methods
+my_ecar = ElectricCar('tesla', 'model s', 2019)
+
+my_ecar.charge()
+my_ecar.drive()
